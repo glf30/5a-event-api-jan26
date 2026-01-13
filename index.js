858a3c2134350d20c681b768a8711bafb4fcdff0
glf30 <greyson.frazier@gmail.com>
2026-01-13 10:41:01 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const logger = require("morgan");
+const connectToMongoDB = require("./database/connectToMongoDB")
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.json())
+app.use(logger("dev"))
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on PORT: ${PORT}`);
+    connectToMongoDB();
+})
